abd537cc93ce897626f5b83d04e4acfc00c15e93
elifgulinan <elifgulinann@gmail.com>
2026-03-01 00:42:27 +0300

    JSON ASCII fix Turkce karakter

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask_cors import CORS
 from crawler import scrape_seo
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 CORS(app)
 
 MISTRAL_API_KEY = os.environ.get("MISTRAL_API_KEY", "")
